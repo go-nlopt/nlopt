1ea4d778a465c81fb11862ab5f14d0e82c612b51
Maciej Bogus <mbogus@users.noreply.github.com>
2020-11-28 19:56:45 +0000

    Install NLopt into /usr

--- install_nlopt.sh
+++ install_nlopt.sh
@@ -2,4 +2,4 @@
 set -ex
 wget https://codeload.github.com/stevengj/nlopt/tar.gz/v2.7.0
 tar -xzvf v2.7.0
-cd nlopt-2.7.0 && mkdir build && cd build && cmake .. && make && sudo make install && cd ../..
+cd nlopt-2.7.0 && mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=/usr .. && make && sudo make install && cd ../..
