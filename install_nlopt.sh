f65843f8e276d326d4e8b6f7787c9b90340d6841
Maciej Bogus <mbogus@users.noreply.github.com>
2020-11-28 16:47:42 +0000

    Revert "Install current version of NLopt library"
    This reverts commit 88ab689c

--- install_nlopt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -ex
-wget https://codeload.github.com/stevengj/nlopt/tar.gz/v2.7.0
-tar -xzvf v2.7.0
-cd nlopt-2.7.0 && mkdir build && cd build && cmake .. && make && sudo make install && cd ../..
-rm -f v2.7.0
-rm -rf nlopt-2.7.0
